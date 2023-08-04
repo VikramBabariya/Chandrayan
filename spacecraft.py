398c231dff0e17625987fad0948861005bdde2af
Vikram Babariya <vikram000712345@gmail.com>
2023-08-04 10:02:57 +0530

    Add unit tests for move method which is added in SpaceCraft Class to move the craft in its facing direction.

--- spacecraft.py
+++ spacecraft.py
@@ -5,5 +5,19 @@ class SpaceCraft:
         self.z = z
         self.face_dir = face_dir
 
+    def move(self, f_or_b):
+        if self.face_dir == "N":
+            self.y += 1 if f_or_b == "f" else -1
+        elif self.face_dir == "S":
+            self.y += -1 if f_or_b == "f" else 1
+        elif self.face_dir == "E":
+            self.x += 1 if f_or_b == "f" else -1
+        elif self.face_dir == "W":
+            self.x += -1 if f_or_b == "f" else 1
+        elif self.face_dir == "U":
+            self.z += 1 if f_or_b == "f" else -1
+        elif self.face_dir == "D":
+            self.z += -1 if f_or_b == "f" else 1
+
     def __str__(self):
         return "Final Position: ({}, {}, {})\nFinal Direction: {}".format(self.x, self.y, self.z, self.face_dir)
--- test_spacecraft.py
+++ test_spacecraft.py
@@ -6,9 +6,53 @@ class TestSpaceCraft(unittest.TestCase):
 
     def setUp(self):
         self.craft1 = SpaceCraft(0, 0, 0, "N")
+        self.craft2 = SpaceCraft(9, 4, -3, "S")
+        self.craft3 = SpaceCraft(4, -6, -2, "E")
+        self.craft4 = SpaceCraft(1, 2, 5, "W")
+        self.craft5 = SpaceCraft(-5, 0, 5, "U")
+        self.craft6 = SpaceCraft(-5, 7, -5, "D")
 
     def test_print_position_direction(self):
         self.assertEqual(self.craft1.__str__(), "Final Position: (0, 0, 0)\nFinal Direction: N")
+
+        self.assertEqual(self.craft2.__str__(), "Final Position: (9, 4, -3)\nFinal Direction: S")
+
+    def test_move(self):
+        self.craft1.move("f")
+        self.assertEqual(self.craft1.__str__(), "Final Position: (0, 1, 0)\nFinal Direction: N")
+
+        self.craft1.move("b")
+        self.assertEqual(self.craft1.__str__(), "Final Position: (0, 0, 0)\nFinal Direction: N")
+
+        self.craft2.move("f")
+        self.assertEqual(self.craft2.__str__(), "Final Position: (9, 3, -3)\nFinal Direction: S")
+
+        self.craft2.move("b")
+        self.assertEqual(self.craft2.__str__(), "Final Position: (9, 4, -3)\nFinal Direction: S")
+
+        self.craft3.move("f")
+        self.assertEqual(self.craft3.__str__(), "Final Position: (5, -6, -2)\nFinal Direction: E")
+
+        self.craft3.move("b")
+        self.assertEqual(self.craft3.__str__(), "Final Position: (4, -6, -2)\nFinal Direction: E")
+
+        self.craft4.move("f")
+        self.assertEqual(self.craft4.__str__(), "Final Position: (0, 2, 5)\nFinal Direction: W")
+
+        self.craft4.move("b")
+        self.assertEqual(self.craft4.__str__(), "Final Position: (1, 2, 5)\nFinal Direction: W")
+
+        self.craft5.move("f")
+        self.assertEqual(self.craft5.__str__(), "Final Position: (-5, 0, 6)\nFinal Direction: U")
+
+        self.craft5.move("b")
+        self.assertEqual(self.craft5.__str__(), "Final Position: (-5, 0, 5)\nFinal Direction: U")
+
+        self.craft6.move("f")
+        self.assertEqual(self.craft6.__str__(), "Final Position: (-5, 7, -6)\nFinal Direction: D")
+
+        self.craft6.move("b")
+        self.assertEqual(self.craft6.__str__(), "Final Position: (-5, 7, -5)\nFinal Direction: D")
 
 
 if __name__ == "__main__":
